bd038fadb0dcfb57c944ee949cb435c907169b7b
Valyxe <bandboy@comcast.net>
2013-05-21 11:28:50 -0400

    Initial commit

---
+++ Initiative Tracker/InitiativeNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class InitiativeNode
+    {
+        private String charName;
+        private int initNum;
+        private InitiativeNode next;
+
+        public InitiativeNode(String name, int num, InitiativeNode prev)
+        {
+            charName = name;
+            initNum = num;
+            next = null;
+            prev.setNext(this);
+        }
+
+        public void setNext(InitiativeNode node)
+        {
+            next = node;
+        }
+
+        public String getCharName()
+        {
+            return charName;
+        }
+
+        public int getInit()
+        {
+            return initNum;
+        }
+
+        public InitiativeNode getNext()
+        {
+            return next;
+        }
+    }
+}
